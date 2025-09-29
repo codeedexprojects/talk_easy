b909700a504adeb91830e70e6e8b9af45282a3c7
Prethewram <prithewramprithew1@gmail.com>
2025-09-29 16:49:50 +0530

    nnss

--- talkeasy/settings.py
+++ talkeasy/settings.py
@@ -46,7 +46,7 @@ INSTALLED_APPS = [
     'channels',
     'calls',
     'payments',
-    'rest_framework'
+    ''
 ]
 
 MIDDLEWARE = [
@@ -144,28 +144,13 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 #databases
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.mysql',
-#         'NAME': 'talk_easy',
-#         'USER': 'root',
-#         'PASSWORD':'@1234567890',
-#         'HOST':'127.0.0.1',
-#         'PORT':'3306',
-#         'OPTIONS': {
-#             'charset': 'utf8mb4',  
-#             'use_unicode': True,
-#         },
-#     }
-# }
-
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'talkeasy$talk_easy',
-        'USER': 'talkeasy',
+        'NAME': 'talk_easy',
+        'USER': 'root',
         'PASSWORD':'@1234567890',
-        'HOST':'talkeasy.mysql.pythonanywhere-services.com',
+        'HOST':'127.0.0.1',
         'PORT':'3306',
         'OPTIONS': {
             'charset': 'utf8mb4',  
@@ -173,6 +158,21 @@ DATABASES = {
         },
     }
 }
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.mysql',
+#         'NAME': 'talkeasy$talk_easy',
+#         'USER': 'talkeasy',
+#         'PASSWORD':'@1234567890',
+#         'HOST':'talkeasy.mysql.pythonanywhere-services.com',
+#         'PORT':'3306',
+#         'OPTIONS': {
+#             'charset': 'utf8mb4',  
+#             'use_unicode': True,
+#         },
+#     }
+# }
 
 
 
