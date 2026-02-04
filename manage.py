b2977a1d57a59829b3cf0b08164b3e87d434d728
Muhammed Fazal <muhammedfazalcholayil@gmail.com>
2026-02-04 14:13:56 +0530

    Fix ASGI_APPLICATION and settings module

--- manage.py
+++ manage.py
@@ -6,7 +6,10 @@ import sys
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'talkeasy.settings')
+    os.environ.setdefault(
+        'DJANGO_SETTINGS_MODULE',
+        'talkeasy.settings.production'
+    )
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
